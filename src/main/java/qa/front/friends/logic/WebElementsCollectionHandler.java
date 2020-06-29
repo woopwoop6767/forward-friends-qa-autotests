8847c0eb08b8f646f54bbf9627252cebac048cf3
Trofim <woopwoop6767@gmail.com>
2020-06-29 13:31:21 +0400

    Fix interface WebElementsCollectionHandler.java

--- src/main/java/qa/front/friends/logic/WebElementsCollectionHandler.java
+++ src/main/java/qa/front/friends/logic/WebElementsCollectionHandler.java
@@ -12,7 +12,7 @@ public interface WebElementsCollectionHandler {
 
     default SelenideElement getFirstMatchByTextElement(ElementsCollection elementsCollection, String elementByText) {
        return elementsCollection.shouldBe(CollectionCondition.sizeGreaterThan(0)).stream()
-                .filter(var -> var.shouldBe(Condition.visible).getText().contains(elementByText))
+                .filter(var -> var.shouldBe(Condition.visible).getText().trim().toLowerCase().contains(elementByText))
                 .findFirst().orElseThrow(NoSuchElementException::new);
     }
 }
